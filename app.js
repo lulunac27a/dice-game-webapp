489be18b44279f9ab94e8327fd4dc36ef63f75cf
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-11-01 15:34:08 -0500

    Format code

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const numberOfDiceInput = document.getElementById('numDice');
-const rollDiceButton = document.getElementById('rollDice');
-const resultText = document.getElementById('result');
+const numberOfDiceInput = document.getElementById("numDice");
+const rollDiceButton = document.getElementById("rollDice");
+const resultText = document.getElementById("result");
 let total = 0;
 let diceRolls = [];
-rollDiceButton.addEventListener('click', () => {
-    const numDice = numberOfDiceInput.value;
-    total = 0;
-    diceRolls = [];
-    for (let i = 0; i < numDice; i++) {
-        const roll = Math.floor(Math.random() * 6) + 1;
-        diceRolls.push(roll);
-        total += roll;
-    }
-    resultText.innerText = `Dice Rolls: ${diceRolls.join(', ')}
+rollDiceButton.addEventListener("click", () => {
+  const numDice = numberOfDiceInput.value;
+  total = 0;
+  diceRolls = [];
+  for (let i = 0; i < numDice; i++) {
+    const roll = Math.floor(Math.random() * 6) + 1;
+    diceRolls.push(roll);
+    total += roll;
+  }
+  resultText.innerText = `Dice Rolls: ${diceRolls.join(", ")}
 Total Roll: ${total}`;
-})
+});
