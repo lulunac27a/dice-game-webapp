533a2a5c4a2666ef2357459b16423e9916bb18b1
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-11-01 15:34:30 -0500

    Add .prettierrc and format code

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const resultText = document.getElementById("result");
 let total = 0;
 let diceRolls = [];
 rollDiceButton.addEventListener("click", () => {
-  const numDice = numberOfDiceInput.value;
-  total = 0;
-  diceRolls = [];
-  for (let i = 0; i < numDice; i++) {
-    const roll = Math.floor(Math.random() * 6) + 1;
-    diceRolls.push(roll);
-    total += roll;
-  }
-  resultText.innerText = `Dice Rolls: ${diceRolls.join(", ")}
+    const numDice = numberOfDiceInput.value;
+    total = 0;
+    diceRolls = [];
+    for (let i = 0; i < numDice; i++) {
+        const roll = Math.floor(Math.random() * 6) + 1;
+        diceRolls.push(roll);
+        total += roll;
+    }
+    resultText.innerText = `Dice Rolls: ${diceRolls.join(", ")}
 Total Roll: ${total}`;
 });
