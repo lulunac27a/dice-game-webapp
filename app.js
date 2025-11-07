7a1594b50af4c0a0f53774ab7c28e8be634b7572
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-11-07 16:17:36 -0600

    Add dice mode selection

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const numberOfDiceInput = document.getElementById("numDice"); //number of dice input
 const numberOfSidesInput = document.getElementById("numSides"); //number of sides input
 const rollDiceButton = document.getElementById("rollDice"); //roll dice button
+const diceModeSelection = document.getElementById("diceMode"); //dice mode selection
 const resultText = document.getElementById("result"); //result output text
 let total = 0; //set total to 0
 let diceRolls = []; //set list of dice rolls to empty list
 rollDiceButton.addEventListener("click", () => {
     //when roll dice button is clicked
     const numDice = parseInt(numberOfDiceInput.value, 10); //get number of dice input value as integer
-    const numSides = parseInt(numberOfSidesInput.value, 10); //get number of sides input value as integer
+    const numSides =
+        diceModeSelection.value === "custom"
+            ? parseInt(numberOfSidesInput.value, 10)
+            : 6; //get number of sides input value as integer based on selected dice mode
     total = 0; //reset total to 0
     diceRolls = []; //reset dice rolls to empty list
     for (let i = 0; i < numDice; i++) {
